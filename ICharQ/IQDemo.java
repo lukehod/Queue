994331084ca9e37b670ef90a2b498104328e1f29
Luke <hodnett53@gmail.com>
2015-08-24 14:09:28 -0500

    add FixedQueue class

---
+++ ICharQ/IQDemo.java
@@ -0,0 +1,31 @@
+// A fixed-size queue class for characters
+class FixedQueue implements ICharQ {
+  private char q[];           // This array holds the queue
+  private int putloc, getloc; // The put and get indicies
+
+  // Construct an empty queue given its size.
+  public FixedQueue(int size) {
+    q = new char[size]; // allocate memory for queue
+    putloc = getloc = 0;
+  }
+
+  // Put a character into the queue
+  public void put(char ch) {
+    if(putloc == q.length) {
+      System.out.println(" - Queue is full.");
+      return;
+    }
+
+    q[putloc++] = ch
+  }
+
+  // Get a character from the queue
+  public char get() {
+    if(putloc == getloc) {
+      System.out.println(" - Queue is empty.");
+      return (char) 0;
+    }
+
+    return q[getloc++];
+  }
+}
